124a333aea8cf5c6acdc83130d37c70693cc66b9
Felipe Barros <felipebarros.engh@gmail.com>
2024-04-30 11:37:33 -0300

    style: create list item component style

---
+++ src/app/components/list-item/list-item.component.ts
@@ -0,0 +1,10 @@
+import {Component } from '@angular/core';
+
+@Component({
+  selector: 'mon-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-item.component.html',
+  styleUrl: './list-item.component.scss',
+})
+export class ListItemComponent { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { ListItemComponent } from '../../components/list-item/list-item.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ListItemComponent
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent {
-
+  mons = [
+    { name: "Pikachu", anime: "Pokémon" },
+    { name: "Agumon", anime: "Digimon" },
+    { name: "Drago", anime: "Bakugan" },
+    { name: "Charmander", anime: "Pokémon" },
+    { name: "Gabumon", anime: "Digimon" },
+    { name: "Hydranoid", anime: "Bakugan" }
+  ];
 }
